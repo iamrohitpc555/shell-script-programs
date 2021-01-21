cce6672a6ee3ade9d610bb2dc5d41b0dfefafd2b
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-01-21 16:34:56 +0530

    Update sumof3.sh

--- Day 7/sumof3.sh
+++ Day 7/sumof3.sh
@@ -1,21 +1,17 @@
-#!/bin/bash
-
-arr_num=( 0 -1 2 -3 1 )
-echo "${arr_num[@]}"
-
-for (( i=0; i<${#arr_num[@]}-2; i++ ))
+#!/bin/bash -x
+triplet="Triplets are";
+arr=( 1 3 -4 0 2 -1 -5 7 -2 5 );
+for(( i=0;i<${#arr[@]}-2;i++ ))
 do
-for (( j=$i+1; j<${#arr_num[@]}-1; j++ ))
-do
-for(( k=$j+1; k<${#arr_num[@]}; k++ ))
-do
-if [[ $(( $((
-${arr_num[i]}+${arr_num[j]} ))+${arr_num[k]} )) == 0 ]]
-then
-echo "sum of integer
-adds upto zero is ${arr_num[i]} ${arr_num[j]}
-${arr_num[k]}"
-fi
+	for(( j=$i+1;j<${#arr[@]}-1;j++ ))
+	do
+		for(( k=$j+1;k<${#arr[@]};k++ ))
+		do
+			if [[ ${arr[i]}+${arr[j]}+${arr[k]} -eq 0 ]]
+			then
+				triplet=$triplet'\n'${arr[i]}" "${arr[j]}" "${arr[k]};
+			fi
+		done
+	done
 done
-done
-done
+echo | sed "i$triplet";
